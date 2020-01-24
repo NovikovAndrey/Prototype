b2359e532377089b1f5d66291912a688d756ef6a
Андрей Новиков <novikov.andrey.v@hotmail.com>
2020-01-24 06:31:39 +0300

    Add project files.

---
+++ Prototype/ConcretePrototype1.cs
@@ -0,0 +1,14 @@
+﻿namespace Prototype
+{
+    internal class ConcretePrototype1 : Prototype
+    {
+        public ConcretePrototype1(int id) : base(id)
+        {
+        }
+
+        public override Prototype Clone()
+        {
+            return new ConcretePrototype1(Id);
+        }
+    }
+}
